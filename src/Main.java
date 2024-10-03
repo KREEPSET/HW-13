9336b7e72dbd4d0d2f5c6d418f2327eeab0703f1
KREEPSET <alex.kotov9991@gmail.com>
2024-10-03 12:16:04 +0300

    HW-13

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Author author1 = new Author("Дэн", "Абнетт");
+        Author author2 = new Author("Грэм", "Макнилл");
+        Book book1 = new Book("Возвышение Хоруса", author1, 2006);
+        Book book2 = new Book("Лживые боги", author2, 2007);
+        book1.setYear(2008);
+        System.out.println("book1.getYear() = " + book1.getYear());
     }
 }
+
